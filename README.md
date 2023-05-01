21eeb2f764d4d9275a605f17cae37de90afb72a8
Carlos Lang <carlos.a.lang@intel.com>
2023-05-01 16:17:49 -0600

    initial version of the utility to extract graph features information from any graph

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # yedparse
-Extracting features from a yEd graphml file, using Swi Prolog.
+Extraction of features from a yEd graphml file, using SWI Prolog.
+
+## Motivation
+We are looking for
+- an easy, agile way to produce graphs, as in "nodes connected through edges" with some GUI, and later
+- be able to extract and process the information stored in the graph
+
+Information to be processed includes
+- node information: their labels and any other property
+- edge information: which nodes are connected to other and what properties those connections have.
+
+## Requirements
+- [yEd](https://www.yworks.com/products/yed)
+- [SWI-Prolog](https://www.swi-prolog.org/)
+
+## Tool selection
+For the GUI, we chose [yEd](https://www.yworks.com/products/yed)
+
+- feely available
+- available in multiple platforms
+- stores its graphs in very concise XML.
+
+For the extraction and interpretation, [SWI-Prolog](https://www.swi-prolog.org/)
+
+- freely available in multiple platforms and open source (Simplified BSD license)
+- comprehensive set of libraries
+- very easy reading and interpretation of XML files
+- very easy and effective manipulation of relationships
+- widely used in research, education and commercial applications
+
+## Assumptions
+- The graph has a single level, with no layers or compound (grouped) nodes
+
+## Examples
+### basic.graphml, read by readit.pl
+
+## To be done
+- alter the current predicates so that instead of "printing" the graph structure it returns a parsed graph term
+- transform the readit.pl script in a module with a predicate that imports graph features from a given graph file
+
---
+++ readit.pl
@@ -0,0 +1,61 @@
+/** readit - 
+ * 
+ * 
+ */
+
+
+% Show the parsed structure of the whole file.
+show_full :-
+    load_html('basic.graphml', Graphml, []),
+    print_term(Graphml,[]).
+
+
+% Load the file and extract the graph information.
+run :-
+    load_html('basic.graphml', [Graphml], []),
+    interpret_graphml(Graphml).
+
+
+% Interpret the parsed structure for the whole graphml file.
+interpret_graphml( element(graphml, _graphml_prop_list, Graphml_element_list) ) :-
+    interpret_graphml_element_list(Graphml_element_list).
+
+
+% Interpret the list of elements within a graphml.
+interpret_graphml_element_list(Graphml_element_list) :-
+    memberchk(element(graph, _graph_prop_list, Graph_element_list), Graphml_element_list),
+    interpret_graph_element_list(Graph_element_list).
+
+
+% Interpret the list of elements within a graph.
+interpret_graph_element_list([]).
+
+interpret_graph_element_list([H|T]) :-
+    once(interpret_graph_element(H)),
+    interpret_graph_element_list(T).
+
+
+% Interpret graph element: node
+interpret_graph_element( element(node, Node_props, Node_elements) ) :-
+    memberchk(id=Node_id, Node_props),
+    member(element(data, Data_props, Data_elements), Node_elements),
+    member(element('y:ImageNode', _image_props, Image_elements ), Data_elements),
+    member(element('y:NodeLabel', _label_props, Label_elements), Image_elements),
+    member(Node_label, Label_elements),
+    writeln(node(Node_id, Node_label)).
+
+
+% Interpret graph element: edge
+interpret_graph_element( element(edge, Edge_props, Edge_elements) ) :-
+    memberchk(id=Edge_id, Edge_props),
+    memberchk(source=Source_id, Edge_props),
+    memberchk(target=Target_id, Edge_props),
+    member(element(data, Data_props, Data_elements), Edge_elements),
+    member(element(_edge_type, _edge_type_props, Edge_type_elements), Data_elements),
+    member(element('y:EdgeLabel', _label_props, Label_elements), Edge_type_elements),
+    member(Edge_label, Label_elements), atom(Edge_label),
+    writeln(edge(Edge_id, Source_id, Target_id, Edge_label)).
+
+
+% Interpret graph element: any other.
+interpret_graph_element(_).
