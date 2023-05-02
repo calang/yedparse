d30c46da76a81f98b404cfd6cc3b04265ac7459d
Carlos Lang <carlos.a.lang@intel.com>
2023-05-01 22:48:16 -0600

    remove singleton warnings.

--- readit.pl
+++ readit.pl
@@ -1,5 +1,7 @@
 /** readit - Read graph information from a yEd graphml file
  * 
+ * Extraction of features from a yEd graphml file, using SWI Prolog.
+ * 
  * @author Carlos Lang-Sanou
  * 
  */
@@ -22,13 +24,13 @@ run :-
 
 
 % Interpret the parsed structure for the whole graphml file.
-interpret_graphml( element(graphml, _graphml_prop_list, Graphml_element_list) ) :-
+interpret_graphml( element(graphml, _Graphml_prop_list, Graphml_element_list) ) :-
     interpret_graphml_element_list(Graphml_element_list).
 
 
 % Interpret the list of elements within a graphml.
 interpret_graphml_element_list(Graphml_element_list) :-
-    memberchk(element(graph, _graph_prop_list, Graph_element_list), Graphml_element_list),
+    memberchk(element(graph, _Graph_prop_list, Graph_element_list), Graphml_element_list),
     interpret_graph_element_list(Graph_element_list).
 
 
@@ -43,9 +45,9 @@ interpret_graph_element_list([H|T]) :-
 % Interpret graph element: node
 interpret_graph_element( element(node, Node_props, Node_elements) ) :- !,
     memberchk(id=Node_id, Node_props),
-    member(element(data, Data_props, Data_elements), Node_elements),
-    member(element('y:ImageNode', _image_props, Image_elements ), Data_elements),
-    member(element('y:NodeLabel', _label_props, Label_elements), Image_elements),
+    member(element(data, _Data_props, Data_elements), Node_elements),
+    member(element('y:ImageNode', _Image_props, Image_elements ), Data_elements),
+    member(element('y:NodeLabel', _Label_props, Label_elements), Image_elements),
     member(Node_label, Label_elements),
     writeln(node(Node_id, Node_label)).
 
@@ -55,9 +57,9 @@ interpret_graph_element( element(edge, Edge_props, Edge_elements) ) :- !,
     memberchk(id=Edge_id, Edge_props),
     memberchk(source=Source_id, Edge_props),
     memberchk(target=Target_id, Edge_props),
-    member(element(data, Data_props, Data_elements), Edge_elements),
-    member(element(_edge_type, _edge_type_props, Edge_type_elements), Data_elements),
-    member(element('y:EdgeLabel', _label_props, Label_elements), Edge_type_elements),
+    member(element(data, _Data_props, Data_elements), Edge_elements),
+    member(element(_Edge_type, _Edge_type_props, Edge_type_elements), Data_elements),
+    member(element('y:EdgeLabel', _Label_props, Label_elements), Edge_type_elements),
     member(Edge_label, Label_elements), atom(Edge_label),
     writeln(edge(Edge_id, Source_id, Target_id, Edge_label)).
 
