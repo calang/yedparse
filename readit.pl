cfe48d9669bb822f69a683da61c88c30aa2e91da
Carlos Lang <carlos.a.lang@intel.com>
2023-05-02 12:52:25 -0600

    add more pldoc

--- readit.pl
+++ readit.pl
@@ -1,13 +1,13 @@
-/** readit - Read graph information from a yEd graphml file
+/** <module> readit - Read graph information from a yEd graphml file
  * 
  * Extraction of features from a yEd graphml file, using SWI Prolog.
  * 
  * @author Carlos Lang-Sanou
- * 
  */
 
 
-% Show the parsed structure of the whole graphml file.
+%! dump_graph is det
+% Write the parsed structure of the whole graphml file onto a file called 'graph.pl'
 dump_graph :-
     load_html('basic.graphml', Graphml, []),
     open('graph.pl', write, Out),
@@ -17,18 +17,25 @@ dump_graph :-
     writeln(ya).
 
 
-% Load the file and extract the graph information.
+%! run is det
+% Load the file and print the graph information.
 run :-
     load_html('basic.graphml', [Graphml], []),
     interpret_graphml(Graphml).
 
 
-% Interpret the parsed structure for the whole graphml file.
+%! interpret_graphml(++Element:term) is det
+% Interpret the parsed Element structure for the whole graphml file.
+%
+% @arg Element term of the form element(graphml, _Graphml_prop_list, Graphml_element_list)
 interpret_graphml( element(graphml, _Graphml_prop_list, Graphml_element_list) ) :-
     interpret_graphml_element_list(Graphml_element_list).
 
 
+%! interpret_graphml_element_list(++Graphml_element_list:list) is det
 % Interpret the list of elements within a graphml.
+%
+% @arg Graphml_element_list - list of graphml elements
 interpret_graphml_element_list(Graphml_element_list) :-
     % writeln('--- interpret_graphml_element_list ---'),
     keys(Graphml_element_list, Key_list),
@@ -37,11 +44,11 @@ interpret_graphml_element_list(Graphml_element_list) :-
     interpret_graph_element_list(Graph_element_list, Key_list).
 
 
-%! interpret_graph_element_list(++Element_list:list, ++Key_list:list).
+%! interpret_graph_element_list(++Element_list:list, ++Key_list:list) is det
 % Interpret the list of graph elements.
 %
 % @arg Element_list list of graph elements
-% @agr Key_list list of key(From, Attr, Key)
+% @arg Key_list list of key(From, Attr, Key)
 interpret_graph_element_list([], _).
 
 interpret_graph_element_list([H|T], Keys) :-
@@ -51,11 +58,11 @@ interpret_graph_element_list([H|T], Keys) :-
     interpret_graph_element_list(T, Keys).
 
 
-%! interpret_graph_element( ++Element:term, ++Key_list ) is det.
+%! interpret_graph_element( ++Element:term, ++Key_list:list ) is det.
 % Extract and print the features of a graph Element.
 %
 % @arg Element term to be evaluated
-% @agr Key_list list of key(From, Attr, Key)
+% @arg Key_list list of key(From, Attr, Key)
 interpret_graph_element( element(node, Node_props, Node_elements), Key_list ) :- !,
     memberchk(id=Node_id, Node_props),
 
@@ -88,13 +95,19 @@ interpret_graph_element( element(edge, Edge_props, Edge_elements), Key_list ) :-
 interpret_graph_element(_,_).
 
 
+%! data(+Key:atom, ++Elements, -Sub_elements) is det
+% Extract the Sub_elements content from a data element in Elements with a given Key
+%
+% @arg Key used to select the desired data element
+% @arg Elements list of elements to search data from
+% @arg Sub_elements content of the data element in Elements with the given Key
 data(Key, Elements, Sub_elements) :-
     member(element(data, Props, Sub_elements), Elements),
     member(key=Key, Props),
     !.
 
 
-%! keys(++Elements:list, -Keys:list) :-
+%! keys(++Elements:list, -Keys:list) is det
 % Extract Keys of interest from Elements
 %
 % @arg Elements list of elements to pull keys from
