f36c91af53912bd55c43c5ce753d27933379f4fe
Carlos Lang <carlos.a.lang@intel.com>
2023-05-02 07:29:24 -0600

    extract keys from graphml elements

--- readit.pl
+++ readit.pl
@@ -7,7 +7,7 @@
  */
 
 
-% Show the parsed structure of the whole file.
+% Show the parsed structure of the whole graphml file.
 dump_graph :-
     load_html('basic.graphml', Graphml, []),
     open('graph.pl', write, Out),
@@ -30,34 +30,51 @@ interpret_graphml( element(graphml, _Graphml_prop_list, Graphml_element_list) ) 
 
 % Interpret the list of elements within a graphml.
 interpret_graphml_element_list(Graphml_element_list) :-
+    writeln('--- interpret_graphml_element_list ---'),
+    keys(Graphml_element_list, Key_list),
+    format('Key_list: ~w~n', [Key_list]),
     memberchk(element(graph, _Graph_prop_list, Graph_element_list), Graphml_element_list),
-    interpret_graph_element_list(Graph_element_list).
+    interpret_graph_element_list(Graph_element_list, Key_list).
 
 
-%! interpret_graph_element_list(++List:list).
+%! interpret_graph_element_list(++Element_list:list, ++Key_list:list).
 % Interpret the list of graph elements.
 %
-% @arg List list of graph elements
-interpret_graph_element_list([]).
+% @arg Element_list list of graph elements
+% @agr Key_list list of key(From, Attr, Key)
+interpret_graph_element_list([], _).
 
-interpret_graph_element_list([H|T]) :-
-    interpret_graph_element(H),
-    interpret_graph_element_list(T).
+interpret_graph_element_list([H|T], Keys) :-
+    writeln('--- interpret_graph_element_list ---'),
+    format('H: ~w~n', [H]),
+    interpret_graph_element(H, Keys),
+    interpret_graph_element_list(T, Keys).
 
 
-%! interpret_graph_element( ++Element:term ) is det.
+%! interpret_graph_element( ++Element:term, ++Key_list ) is det.
 % Extract and print the features of a graph Element.
 %
 % @arg Element term to be evaluated
-interpret_graph_element( element(node, Node_props, Node_elements) ) :- !,
+% @agr Key_list list of key(From, Attr, Key)
+interpret_graph_element( element(node, Node_props, Node_elements), Key_list ) :- !,
+    writeln('--- interpret_graph_element ---'),
+    format('Key_list: ~w~n', [Key_list]),
     memberchk(id=Node_id, Node_props),
+    % memberchk(key(node, description, Key_node_description), Key_list),
+    % data(Key_node_description, Node_elements, Node_description),
+
     member(element(data, _Data_props, Data_elements), Node_elements),
     member(element('y:ImageNode', _Image_props, Image_elements ), Data_elements),
     member(element('y:NodeLabel', _Label_props, Label_elements), Image_elements),
     member(Node_label, Label_elements),
-    writeln(node(Node_id, Node_label)).
+    writeln(node(Node_id, Node_label, Node_description)).
 
-interpret_graph_element( element(edge, Edge_props, Edge_elements) ) :- !,
+data(Key, Elements, Sub_elements) :-
+    member(element(data, Props, Sub_elements), Elements),
+    member([key=Key], Props),
+    !.
+
+interpret_graph_element( element(edge, Edge_props, Edge_elements), Key_list ) :- !,
     memberchk(id=Edge_id, Edge_props),
     memberchk(source=Source_id, Edge_props),
     memberchk(target=Target_id, Edge_props),
@@ -67,23 +84,47 @@ interpret_graph_element( element(edge, Edge_props, Edge_elements) ) :- !,
     member(Edge_label, Label_elements), atom(Edge_label),
     writeln(edge(Edge_id, Source_id, Target_id, Edge_label)).
 
-interpret_graph_element(_).
+interpret_graph_element(_, _).
 
 
-%! key(++Elements:list, +Key_name:atom, -Key) is det.
-% Extract the Key associated with a Key_Name
+%! keys(++Elements:list, -Keys:list) :-
+% Extract Keys of interest from Elements
+%
+% @arg Elements list of elements to pull keys from
+% @arg Keys list of terms key(For, Attr, Key)
+keys(Elements, Keys) :-
+    findall(
+        key(For, Attr, Key),
+        (
+            member(
+                (For, Attr),
+                [
+                    (node, nodegraphics)
+                    ,(node, description)
+                    ,(edge, edgegraphics)
+                    ,(edge, description)
+                ]
+            ),
+            key(Elements, For, Attr, Key)
+        ),
+        Keys
+    ).
+
+
+%! key(++Elements:list, +For:atom, +Attr_name:atom, -Key) is det.
+% Extract the Key used for the Attr_name of the For type of element.
 %
 % @arg Elements list of graphml elements
-% @arg Key_name one of node_description, node_graphics, ...
-key(Elements, node_description, Key) :-
-    memberchk(element(key, Key_props, _), Elements),
-    memberchk(for=node, Key_props),
-    memberchk('attr.name'=description, Key_props),
-    memberchk(id=Key, Key_props).
+% @arg For type of element as described in its propertied
+% @arg Attr_name name of the attribute of interest
+% @arg Key used for the corresponding For type of element and given Attr_name
+key(Elements, For, Attr_name, Key) :-
+    member(element(key, Key_props, _), Elements),
+    member(for=For, Key_props),
+    (   member('attr.name'=Attr_name, Key_props)
+    ;   member('yfiles.type'=Attr_name, Key_props)
+    ),
+    member(id=Key, Key_props),
+    !.
 
-key(Elements, node_graphics, Key) :-
-    memberchk(element(key, Key_props, _), Elements),
-    memberchk(for=node, Key_props),
-    memberchk('yfiles.type'=nodegraphics, Key_props),
-    memberchk(id=Key, Key_props).
 
